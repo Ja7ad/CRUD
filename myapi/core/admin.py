e3670257dfe24f9c59a6eb55d636561cbf900910
kavehkm <mehrbaniankaveh@gmail.com>
2019-04-17 11:33:19 +0430

    register core app models in admin site

--- myapi/core/admin.py
+++ myapi/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Category, Ad
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'tell', 'category', 'owner', 'created')
+    list_filter = ('category', 'created')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Ad, AdAdmin)
