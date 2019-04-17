48e363a7ceef702cf6fcf14246ac1d650db9fe8a
kavehkm <mehrbaniankaveh@gmail.com>
2019-04-17 11:58:21 +0430

    create core app view

--- myapi/core/views.py
+++ myapi/core/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from core import serializers
+from core.models import User, Category, Ad
+
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+
+
+
+class Root(APIView):
+    def get(self, request):
+        content = {
+            'ad_list': reverse('ad_list', request=request),
+            'category_list': reverse('category_list', request=request),
+            'user_list': reverse('user_list', request=request)
+        }
+        return Response(content)
+
+
+class Signup(APIView):
+    def post(self, request):
+        serializer = serializers.UserCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            token = Token.objects.create(user=new_user).key
+            content = {
+                'user': serializer.data,
+                'token': token
+            }
+            return Response(content, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AdList(generics.ListCreateAPIView):
+    queryset = Ad.objects.all().order_by('-created')
+    serializer_class = serializers.AdSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return serializers.AdCreateSerializer
+        return self.serializer_class
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class AdDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ad.objects.all()
+    serializer_class = serializers.AdSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'PUT':
+            return serializers.AdCreateSerializer
+        return self.serializer_class
+
+
+class CategoryList(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+
+class AdsByCategory(generics.ListAPIView):
+    queryset = Ad.objects.all().order_by('-created')
+    serializer_class = serializers.AdSerializer
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return self.queryset.filter(category=category)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class AdsByUser(generics.ListAPIView):
+    queryset = Ad.objects.all().order_by('-created')
+    serializer_class = serializers.AdSerializer
+
+    def get_queryset(self):
+        user = get_object_or_404(User, pk=self.kwargs.get('pk'))
+        return self.queryset.filter(owner=user)
