8d61b2744f6e5f99279d5e567504697c644f3955
kavehkm <mehrbaniankaveh@gmail.com>
2019-04-17 11:32:50 +0430

    create core app models(category, ad)

--- myapi/core/models.py
+++ myapi/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Ad(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='images/')
+    price = models.PositiveIntegerField()
+    tell = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='ads')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads')
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s-%d" % (self.title, self.pk)
+
