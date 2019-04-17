5178d72b9c43757d85f09648c00c77d0b7b1dcf5
kavehkm <mehrbaniankaveh@gmail.com>
2019-04-17 16:16:32 +0430

    update core app urls(trim slashes)

--- myapi/core/urls.py
+++ myapi/core/urls.py
@@ -5,15 +5,15 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.Root.as_view(), name='root'),
-    path('accounts/signup/', views.Signup.as_view(), name='signup'),
-    path('accounts/signin/', obtain_auth_token, name='signin'),
+    path('accounts/signup', views.Signup.as_view(), name='signup'),
+    path('accounts/signin', obtain_auth_token, name='signin'),
 
-    path('ads/', views.AdList.as_view(), name='ad_list'),
-    path('ad/<int:pk>/', views.AdDetail.as_view(), name='ad_detail'),
+    path('ads', views.AdList.as_view(), name='ad_list'),
+    path('ad/<int:pk>', views.AdDetail.as_view(), name='ad_detail'),
 
-    path('categories/', views.CategoryList.as_view(), name='category_list'),
-    path('category/<int:pk>/ads/', views.AdsByCategory.as_view(), name='ads_by_category'),
+    path('categories', views.CategoryList.as_view(), name='category_list'),
+    path('category/<int:pk>/ads', views.AdsByCategory.as_view(), name='ads_by_category'),
 
     path('users/', views.UserList.as_view(), name='user_list'),
-    path('user/<int:pk>/ads/', views.AdsByUser.as_view(), name='ads_by_user'),
+    path('user/<int:pk>/ads', views.AdsByUser.as_view(), name='ads_by_user'),
 ]
